65199ab9a56811aaf8b227dfc6d4ce3cc2aea917
Vinicius <vinicius.01deagosto@gmail.com>
2025-12-28 13:29:19 -0300

    feature: 0811 validate token

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from './Login.module.css';
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LoginForm from "./Components/LoginForm/LoginForm";
 import LoginCreate from "./Components/LoginCreate/LoginCreate";
 import LoginPasswordLost from "./Components/LoginPasswordLost/LoginPasswordLost";
 import LoginPasswordReset from "./Components/LoginPasswordReset/LoginPasswordReset";
+import { UserContext } from "../../Contexts/UserContext";
 
 const Login = () => {
+    const {login} = React.useContext(UserContext)
+    if(login) return <Navigate to="/conta" />;
+
     return <div>
         <Routes>
             <Route path="/" element={<LoginForm />} />
--- src/Contexts/UserContext.jsx
+++ src/Contexts/UserContext.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TOKEN, USER } from "../Endpoints/Api";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = React.createContext()
 
@@ -8,6 +9,42 @@ export const UserStorage = ({children}) => {
     const [login, setLogin] = React.useState(null);
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState(false);
+    const navigate = useNavigate();
+    const userLogout = React.useCallback( async function() {
+        setData(null)
+        setError(null)
+        setLoading(false)
+        setLogin(false)
+        window.localStorage.removeItem('token')
+        navigate('/login');
+    }, [navigate])
+
+    async function getUser(token) {
+        const {url, options} = USER.GET(token)
+        const response = await fetch(url, options);
+        const json = await response.json();
+        setData(json);
+        setLogin(true);
+    }
+
+    async function userLogin(username, password) {
+        try {
+            setError(null)
+            setLoading(true)
+            const {url, options} = TOKEN.POST({username, password})
+            const tokenResponse = await fetch(url, options)
+            if(!tokenResponse.ok) throw new Error(`Error: ${tokenResponse.status}`)
+            const {token} = await tokenResponse.json();
+            window.localStorage.setItem('token', token);
+            await getUser(token);
+            navigate('/conta');
+        } catch(error) {
+            setError(error.message);
+            setLogin(false);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     React.useEffect(() => {
         async function autoLogin() {
@@ -27,42 +64,7 @@ export const UserStorage = ({children}) => {
             }
         }
         autoLogin();
-    }, [])
-
-    async function getUser(token) {
-        const {url, options} = USER.GET(token)
-        const response = await fetch(url, options);
-        const json = await response.json();
-        setData(json);
-        setLogin(true);
-        
-    }
-
-    async function userLogin(username, password) {
-        try {
-            setError(null)
-            setLoading(true)
-            const {url, options} = TOKEN.POST({username, password})
-            const tokenResponse = await fetch(url, options)
-            if(!tokenResponse.ok) throw new Error(`Error: ${tokenResponse.status}`)
-            const {token} = await tokenResponse.json();
-            window.localStorage.setItem('token', token);
-            await getUser(token);
-        } catch(error) {
-            setError(error.message);
-            setLogin(false);
-        } finally {
-            setLoading(false);
-        }
-    }
-
-    async function userLogout() {
-        setData(null)
-        setError(null)
-        setLoading(false)
-        setLogin(false)
-        window.localStorage.removeItem('token')
-    }
+    }, [userLogout])
 
     return (
         <UserContext.Provider value={{userLogin, userLogout, data, error, loading, login}}>
