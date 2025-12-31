9deae02a0c7766d325bd018248608735a8e61514
Vinicius <vinicius.01deagosto@gmail.com>
2025-12-30 23:27:05 -0300

    feature: 0812 login form

--- src/Components/Login/Components/LoginForm/LoginForm.jsx
+++ src/Components/Login/Components/LoginForm/LoginForm.jsx
@@ -3,6 +3,11 @@ import Input from "../../../Shared/Forms/Components/Input/Input";
 import Button from "../../../Shared/Forms/Components/Button/Button";
 import useForm from "../../../../Hooks/useForm";
 import { UserContext } from "../../../../Contexts/UserContext";
+import Error from "../../../Shared/Helpers/Error";
+import styles from './LoginForm.module.css';
+import stylesBtn from '../../../Shared/Forms/Components/Button/Button.module.css';
+
+import { Link } from "react-router-dom";
 
 const LoginForm = () => {
     const username = useForm();
@@ -18,13 +23,18 @@ const LoginForm = () => {
     return (
     <section className="animeLeft">
         <h1 className="title">Login</h1>
-        <form action="" onSubmit={handleSubmit}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <Input label="Usuário" type="text" name="username" {...username}/>
             <Input label="Senha" type="password" name="password" {...password}/>
             {loading ? (<Button disabled>Carregando...</Button>) : (<Button>Entrar</Button>)}
-            
-            {error && <p>{error}</p>}
+            <Error error={error}/>
         </form>
+        <Link className={styles.forget} to="/login/forget">Perdeu a Senha?</Link>
+        <div className={styles.register}>
+            <h2 className={styles.subtitle}>Cadastre-se</h2>
+            <p>Ainda não possui conta?Cadastre-se no site.</p>
+            <Link className={stylesBtn.button} to="/login/criar">Cadastro</Link>
+        </div>
     </section>)
 }
 
